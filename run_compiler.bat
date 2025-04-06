b305aa8e588ca4d17f643bea63fbd7ef7e12793d
ItamarLit <itamarlitvin@gmail.com>
2025-04-06 17:28:42 +0300

    Added bat file for running compiler

---
+++ run_compiler.bat
@@ -0,0 +1,33 @@
+@echo off
+echo Running compiler...
+
+if "%~1"=="" (
+    echo Usage: run_compiler.bat file.forge [-T] [-A] [-S] [-O]
+    exit /b 1
+)
+
+set INPUT=ProjectData\%1
+shift
+set FLAGS=%*
+
+Debug\ForgeCompilerProj.exe %INPUT% %FLAGS%
+
+if %errorlevel% neq 0 (
+    exit /b %errorlevel%
+)
+
+IF EXIST ProjectData\output.asm (
+    echo Assembling...
+    ml64 /c /Fo ProjectData\output.obj ProjectData\output.asm
+
+    echo Linking...
+    link ProjectData\output.obj /entry:Main /subsystem:console kernel32.lib /OUT:ProjectData\output.exe
+
+    IF EXIST ProjectData\output.exe (
+        echo Running program...
+        ProjectData\output.exe
+        del ProjectData\output.obj
+    ) ELSE (
+        echo Linking failed.
+    )
+)
